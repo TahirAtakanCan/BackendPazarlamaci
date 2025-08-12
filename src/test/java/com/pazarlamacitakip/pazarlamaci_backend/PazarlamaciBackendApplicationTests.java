682eb1a23944c28b11faee24078c0d6168e1a1e7
Tahir Atakan Can <atkan4202@gmail.com>
2025-08-12 12:35:27 +0300

    first commit

---
+++ src/test/java/com/pazarlamacitakip/pazarlamaci_backend/PazarlamaciBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pazarlamacitakip.pazarlamaci_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PazarlamaciBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
