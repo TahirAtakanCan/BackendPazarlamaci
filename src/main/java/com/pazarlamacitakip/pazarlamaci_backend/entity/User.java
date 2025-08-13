5eec562c7b385876a5c237544a36c49e3b260b7a
Tahir Atakan Can <atkan4202@gmail.com>
2025-08-13 15:45:38 +0300

    User entity kodları eklendi

--- src/main/java/com/pazarlamacitakip/pazarlamaci_backend/entity/User.java
+++ src/main/java/com/pazarlamacitakip/pazarlamaci_backend/entity/User.java
@@ -15,70 +15,70 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-    
+
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(columnDefinition = "UUID")
     private UUID id;
-    
+
     @Column(name = "adi")
     private String adi;
-    
+
     @Column(name = "sifre")
     private String sifre;
-    
+
     @Column(name = "telefon")
     private String telefon;
-    
+
     @Column(name = "email")
     private String email;
-    
+
     @Column(name = "aciklama")
     private String aciklama;
-    
+
     @Column(name = "aciklama2")
     private String aciklama2;
-    
+
     @Column(name = "adminmi")
     private Boolean adminmi = false;
-    
+
     @Column(name = "aktifmi")
     private Boolean aktifmi = true;
-    
+
     @Column(name = "sirketkodu")
     private String sirketkodu;
-    
+
     @Column(name = "plaka")
     private String plaka;
-    
+
     @Column(name = "km")
     private Double km;
-    
+
     @Column(name = "yer")
     private String yer;
-    
+
     @Column(name = "yetki")
     private String yetki;
-    
+
     @Column(name = "aylik_hedef")
     private Double aylikHedef;
-    
+
     @Column(name = "haftalik_hedef")
     private Double haftalikHedef;
-    
+
     @Column(name = "hedef_ziyaret_sayisi")
     private Integer hedefZiyaretSayisi;
-    
+
     @Column(name = "bolge")
     private String bolge;
-    
+
     @Column(name = "son_giris_tarihi")
     private LocalDateTime sonGirisTarihi;
-    
+
     @Column(name = "gec_tahsilat")
     private Double gecTahsilat;
-    
+
     @Column(name = "gec_tahsilat_guncelleme_tarihi")
     private LocalDateTime gecTahsilatGuncellemeTarihi;
 }
